d2ca91511aba985e7cd9246ec5a8d28b0b8ecb63
Jorge Luis Sampaio de Oliveira <jorgeluis.geek@gmail.com>
2023-06-12 22:07:12 -0300

    criação da div de profile de usuario

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,14 +7,27 @@ function App() {
     <div className="App">
       <Header />
       <div className="conteudo">
-          <img src={background} className="background" alt="background app"/>
-          <div className="info">
-            <div>
-              <input type="text" name="usuario" placeholder="@username"/>
-              <button>Buscas</button>
-            </div>
+        <img src={background} className="background" alt="background app" />
+        {/* Div do Algoritmo de Busca por usuario */}
+        <div className="info">
+          <div>
+            <input type="text" name="usuario" placeholder="@username" />
+            <button>Buscas</button>
           </div>
+        </div>
+        <br></br>
+        <div className="perfil">
+          <img src="https://avatars.githubusercontent.com/u/21243659?v=4" className="profile" alt="imagem de perfil" />
+          <div>
+
+            <h3>Jorge Luis Sampaio de Oliveira</h3>
+            <span>@jorgeluis-dev</span>
+            <p>Descrição: Software Engineer | Java Back-end |</p>
+          </div>
+        </div>
+        <hr />
       </div>
+
     </div>
   );
 }
